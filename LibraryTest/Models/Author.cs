e2796759e613dead22ba01f845f144adb0896115
ncls99 <junico99301@gmail.com>
2024-01-29 12:04:09 -0500

    Crear los modelos Book y Author con sus propiedades

---
+++ LibraryTest/Models/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryTest.Models
+{
+    public class Author
+    {
+        [Key][DatabaseGenerated(DatabaseGeneratedOption.Identity)] public int AuthorId {  get; set; }
+        [Required]public string Name { get; set; }
+
+        public virtual List<Book> BookList { get; set;}
+    }
+}
---
+++ LibraryTest/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryTest.Models
+{
+    public class Book
+    {
+        [Key] public int BookId { get; set; }
+        [Required] public string Title { get; set; }
+        public int AuthorId { get; set; }
+        [ForeignKey("AuthorId")] public virtual Author Author { get; set; }
+    }
+}
