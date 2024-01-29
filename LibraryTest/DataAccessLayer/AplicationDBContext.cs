6734064c5fa35a10e5d8fd9ba3d5f53d60706dc1
ncls99 <junico99301@gmail.com>
2024-01-29 13:02:50 -0500

    Crear context para la conexión la BD y se configuran los StringConnections

---
+++ LibraryTest/DataAccessLayer/AplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using LibraryTest.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LibraryTest.DataAccessLayer
+{
+    public class AplicationDBContext : DbContext
+    {
+        public AplicationDBContext() : base("Library_DataBase")
+        { 
+        
+        }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Book> Book { get; set; }
+    }
+}
